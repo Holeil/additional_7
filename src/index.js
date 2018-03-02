787c15ca83bd508f1e55da2b7e1c30f56f392144
Holeil <holeilaccougm@gmail.com>
2018-03-02 21:08:38 +0300

    Commit 2

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 module.exports = function solveSudoku(matrix) {
-  function Inf(name, num) {
+  function Cell(name, num) {
     this.name = name;
     this.num = num;
-    this.active = "false";
   }
+  //////////////////////////////////////////////////////////////////////////////
   function analize(x, y) {
-    var i = 0;
-    while(i < 9) {
-      var z = 0;
-      while(z < 9) {
+    for(let i = 0; i < 9; i++) {
+      for(let z = 0; z < 9; z++) {
         if((matrix[z][y].name == matrix[x][y].num[i]) ||
         (matrix[x][z].name == matrix[x][y].num[i])) matrix[x][y].num[i] = 0;
-        z++;
       }
-      var q = 0, qc = 3, w = 0, wc = 3;
+      let q = 0, qc = 3, w = 0, wc = 3;
       if(x != 0) {
         q=x-x%3;
         qc=q+3;
@@ -31,33 +28,285 @@ module.exports = function solveSudoku(matrix) {
         q++;
         w-=3;
       }
-      i++;
     }
-    return matrix[x][y].num;
   }
-  /////////////////////////////////
-  function deepAnalize(x,  y) {
-    matrix[x][y].active = "true";
-    var i = 0;
-    while(i < 9) {
+  //////////////////////////////////////////////////////////////////////////////
+  function update(x, y) {
+    let count = 0, n;
+    for(let i = 0; i < 9; i++) {
       if(matrix[x][y].num[i] != 0) {
-        var z = 0, count = 0;
-        while(z < 9) {
-          if((z != x) && (matrix[z][y].name == 0))
-            if(matrix[z][y].num[i] == matrix[x][y].num[i]) count++;
-          if((z != y) && (matrix[x][z].name == 0))
-            if(matrix[x][z].num[i] == matrix[x][y].num[i]) count++;
-          z++;
+        count++;
+        n = matrix[x][y].num[i];
+      }
+    }
+    if(count == 1) {
+      matrix[x][y].name = n;
+      matrix[x][y].num = [];
+    }
+  }
+  function rowAnalize(x, y) {
+    for(let i = 0; i < 9; i++) {
+      if(matrix[x][y].num[i] != 0) {
+        ////////////////////////////////////////////////////////////////////////
+        let y1=undefined, y2=undefined, count = 0;
+        let w = 0, wc = 3;
+        if(y != 0) {
+          w=y-y%3;
+          wc=w+3;
         }
-        if(count == 0) {
-          for(var k in matrix[x][y].num) {
-            if(matrix[x][y].num[k] != matrix[x][y].num[i]) matrix[x][y].num[k] = 0;
+        while(w < wc) {
+          if((w != y) && (matrix[x][w].name == 0) && (matrix[x][w].num[i]==matrix[x][y].num[i])) {
+            if(count == 0) {
+              y1 = w;
+              count++;
+            }
+            if(count == 1) {
+              y2 = w;
+            }
           }
-          i++;
-          continue;
+          w++;
         }
-        else count = 0;
-        var q = 0, qc = 3, w = 0, wc = 3;
+        let r = 0, rc = 3,  t= 0, tc = 3, key = 0;
+        if(x != 0) {
+          r=x-x%3;
+          rc=r+3;
+        }
+        if(y != 0) {
+          t=y-y%3;
+          tc=t+3;
+        }
+        while(r < rc) {
+          while(t < tc) {
+            if(matrix[r][t] != matrix[x][y]) {
+              if((y1 == undefined) || (matrix[r][t] != matrix[x][y1])) {
+                if((y2 == undefined) || (matrix[r][t] != matrix[x][y2])) {
+                  if(matrix[r][t].num[i] == matrix[x][y].num[i]) key = 1;
+                }
+              }
+            }
+            t++;
+          }
+          r++;
+          t-=3;
+        }
+        if(key == 0) {
+          for(var e = 0; e < 9; e++) {
+            if((e != y) && (matrix[x][e].num[i] == matrix[x][y].num[i])) {
+              if((y1 == undefined) || (e != y1)) {
+                if((y2 == undefined) || (e != y2)) {
+                  matrix[x][e].num[i] = 0;
+                }
+              }
+            }
+          }
+        }
+
+        ////////////////////////////////////////////////////////////////////////
+        w = 0; wc = 3; y1 = undefined; y2 = undefined; count = 0;
+        if(y != 0) {
+          w=y-y%3;
+          wc=w+3;
+        }
+
+        while(w < wc) {
+          if((w != y) && (matrix[x][w] == 0) && (matrix[x][w].num[i] == matrix[x][y].num[i])) {
+            if(count == 0) {
+              y1 = w;
+              count++;
+            }
+            if(count == 1) {
+              y2 = w;
+            }
+          }
+          w++;
+        }
+        key = 0;
+        for(let z = 0; z < 9; z++) {
+          if(z != y) {
+            if((y1 == undefined) || (z != y1)) {
+              if((y2 == undefined) || (z != y2)) {
+                if(matrix[x][z].num[i] == matrix[x][y].num[i]) key = 1;
+              }
+            }
+          }
+        }
+        if(key == 0) {
+          let r = 0, rc = 3,  t= 0, tc = 3;
+          if(x != 0) {
+            r=x-x%3;
+            rc=r+3;
+          }
+          if(y != 0) {
+            t=y-y%3;
+            tc=t+3;
+          }
+          while(r < rc) {
+            while(t < tc) {
+              if(matrix[r][t] != matrix[x][y]) {
+                if((y1 == undefined) || (matrix[r][t] != matrix[x][y1])) {
+                  if((y2 == undefined) || (matrix[r][t] != matrix[x][y2])) {
+                    if(matrix[r][t].num[i] == matrix[x][y].num[i]) matrix[r][t].num[i] = 0;
+                  }
+                }
+              }
+              t++;
+            }
+            r++;
+            t-=3;
+          }
+        }
+      }
+    }
+  }
+  //////////////////////////////////////////////////////////////////////////////
+  function columnAnalize(x, y) {
+    for(let i = 0; i < 9; i++) {
+      if(matrix[x][y].num[i] != 0) {
+        ////////////////////////////////////////////////////////////////////////
+        let x1=undefined, x2=undefined, count = 0;
+        let q = 0, qc = 3;
+        if(x != 0) {
+          q=x-x%3;
+          qc=q+3;
+        }
+        while(q < qc) {
+          if((q != y) && (matrix[q][y].name == 0) && (matrix[q][y].num[i]==matrix[x][y].num[i])) {
+            if(count == 0) {
+              x1 = q;
+              count++;
+            }
+            if(count == 1) {
+              x2 = q;
+            }
+          }
+          q++;
+        }
+        let r = 0, rc = 3,  t= 0, tc = 3, key = 0;
+        if(x != 0) {
+          r=x-x%3;
+          rc=r+3;
+        }
+        if(y != 0) {
+          t=y-y%3;
+          tc=t+3;
+        }
+        while(r < rc) {
+          while(t < tc) {
+            if(matrix[r][t] != matrix[x][y]) {
+              if((x1 == undefined) || (matrix[r][t] != matrix[x1][y])) {
+                if((x2 == undefined) || (matrix[r][t] != matrix[x2][y])) {
+                  if(matrix[r][t].num[i] == matrix[x][y].num[i]) key = 1;
+                }
+              }
+            }
+            t++;
+          }
+          r++;
+          t-=3;
+        }
+        if(key == 0) {
+          for(var e = 0; e < 9; e++) {
+            if((e != y) && (matrix[e][y].num[i] == matrix[x][y].num[i])) {
+              if((x1 == undefined) || (e != x1)) {
+                if((x2 == undefined) || (e != x2)) {
+                  matrix[e][y].num[i] = 0;
+                }
+              }
+            }
+          }
+        }
+
+        ////////////////////////////////////////////////////////////////////////
+        q = 0; qc = 3; x1 = undefined; x2 = undefined; count = 0;
+        if(x != 0) {
+          q=x-x%3;
+          qc=q+3;
+        }
+
+        while(q < qc) {
+          if((q != x) && (matrix[q][y] == 0) && (matrix[q][y].num[i] == matrix[x][y].num[i])) {
+            if(count == 0) {
+              x1 = q;
+              count++;
+            }
+            if(count == 1) {
+              x2 = q;
+            }
+          }
+          q++;
+        }
+        key = 0;
+        for(let z = 0; z < 9; z++) {
+          if(z != x) {
+            if((x1 == undefined) || (z != x1)) {
+              if((x2 == undefined) || (z != x2)) {
+                if(matrix[z][y].num[i] == matrix[x][y].num[i]) key = 1;
+              }
+            }
+          }
+        }
+        if(key == 0) {
+          let r = 0, rc = 3,  t= 0, tc = 3;
+          if(x != 0) {
+            r=x-x%3;
+            rc=r+3;
+          }
+          if(y != 0) {
+            t=y-y%3;
+            tc=t+3;
+          }
+          while(r < rc) {
+            while(t < tc) {
+              if(matrix[r][t] != matrix[x][y]) {
+                if((x1 == undefined) || (matrix[r][t] != matrix[x1][y])) {
+                  if((x2 == undefined) || (matrix[r][t] != matrix[x2][y])) {
+                    if(matrix[r][t].num[i] == matrix[x][y].num[i]) matrix[r][t].num[i] = 0;
+                  }
+                }
+              }
+              t++;
+            }
+            r++;
+            t-=3;
+          }
+        }
+      }
+    }
+  }
+  //////////////////////////////////////////////////////////////////////////////
+  function oneInPozition(x, y) {
+    for(let i = 0; i < 9; i++) {
+      if(matrix[x][y].num[i] != 0) {
+        let key = 0;
+        for(let z = 0; z < 9; z++) {
+          if((z != y) && (matrix[x][z].name == 0)) {
+            if(matrix[x][z].num[i] == matrix[x][y].num[i]) {
+              key = 1;
+              break;
+            }
+          }
+        }
+        if(key == 0) {
+          matrix[x][y].name = matrix[x][y].num[i];
+          matrix[x][y].num = [];
+          break;
+        }
+        else key = 0;
+        for(let z = 0; z < 9; z++) {
+          if((z != x) && (matrix[z][y].name == 0)) {
+            if(matrix[z][y].num[i] == matrix[x][y].num[i]) {
+              key = 1;
+              break;
+            }
+          }
+        }
+        if(key == 0) {
+          matrix[x][y].name = matrix[x][y].num[i];
+          matrix[x][y].num = [];
+          break;
+        }
+        else key = 0;
+        let q = 0, qc = 3, w = 0, wc = 3;
         if(x != 0) {
           q=x-x%3;
           qc=q+3;
@@ -68,35 +317,39 @@ module.exports = function solveSudoku(matrix) {
         }
         while(q < qc) {
           while(w < wc) {
-            if((matrix[q][w].name == 0) && (matrix[q][w].active != matrix[x][y].active) &&
-            (matrix[q][w].num[i] == matrix[x][y].num[i])) count++;
+            if((matrix[q][w] != matrix[x][y]) && (matrix[q][w].name == 0)) {
+              if(matrix[q][w].num[i] == matrix[x][y].num[i]) {
+                key = 1;
+                break;
+              }
+            }
             w++;
           }
+          if(key == 1) break;
           q++;
           w-=3;
         }
-        if(count == 0) {
-          for(var k in matrix[x][y].num) {
-            if(matrix[x][y].num[k] != matrix[x][y].num[i]) matrix[x][y].num[k] = 0;
-          }
+        if(key == 0) {
+          matrix[x][y].name = matrix[x][y].num[i];
+          matrix[x][y].num = [];
+          break;
         }
       }
-      i++;
     }
-    matrix[x][y].active = "false";
-    return matrix[x][y].num;
   }
+  //////////////////////////////////////////////////////////////////////////////
   function postAnalize(x, y) {
-    matrix[x][y].active = "true";
-    var z = 0;
-    while(z < 9) {
-      if(matrix[z][y].name == 0)
-        if(matrix[z][y].num[matrix[x][y].name - 1] == matrix[x][y].name) matrix[z][y].num[matrix[x][y].name - 1] = 0;
-      if(matrix[x][z].name == 0)
-        if(matrix[z][y].num[matrix[x][y].name - 1] == matrix[x][y].name) matrix[x][z].num[matrix[x][y].name - 1] = 0;
-      z++;
+    for(let z = 0; z < 9; z++) {
+      if((matrix[x][z] != matrix[x][y]) && (matrix[z][y] != matrix[x][y])) {
+        if(matrix[x][z].num[matrix[x][y].name - 1] == matrix[x][y].name) {
+          matrix[x][z].num[matrix[x][y].name - 1] = 0;
+        }
+        if(matrix[z][y].num[matrix[x][y].name - 1] == matrix[x][y].name) {
+          matrix[z][y].num[matrix[x][y].name - 1] = 0;
+        }
+      }
     }
-    var q = 0, qc = 3, w = 0, wc = 3;
+    let q = 0, qc = 3, w = 0, wc = 3;
     if(x != 0) {
       q=x-x%3;
       qc=q+3;
@@ -107,146 +360,66 @@ module.exports = function solveSudoku(matrix) {
     }
     while(q < qc) {
       while(w < wc) {
-        if(matrix[q][w].name == 0)
-          if(matrix[q][w].num[matrix[x][y].name - 1] == matrix[x][y].name) matrix[q][w].num[matrix[x][y].name - 1] = 0;
+        if(matrix[q][w] != matrix[x][y]) {
+          if(matrix[q][w].num[matrix[x][y].name - 1] == matrix[x][y].name) {
+            matrix[q][w].num[matrix[x][y].name - 1] = 0;
+          }
+        }
         w++;
       }
       q++;
       w-=3;
     }
-    matrix[x][y].active = "false";
   }
-  //////////////////////////////////////
-  function update(x, y) {
-    var k = 0, name = 0;
-    for(var i in matrix[x][y].num) {
-      if(matrix[x][y].num[i] != 0) {
-        k++;
-        name = matrix[x][y].num[i];
-      }
-    }
-    if(k == 1)  {
-      matrix[x][y].num = [];
-      return name;
-    }
-    else if(k > 1) return 0;
-    else "Что то пошло не так!";
-  }
-  function nakedCouples(x, y, array) {
-    matrix[x][y].active = "true";
-    count = 0;
-    for(var i in array) {
-      if(array[i] != 0) count++;
-    }
-    if(count == 2) {
-      ////////////////////По строке
-      var z = 0, n = 0, q = 0, w = 0;
-      while(z < 9) {
-        if((matrix[x][z].name == 0) && (matrix[x][z].active != matrix[x][y].active)) {
-          for(var i = 0; i < array.length; i++) {
-            if(array[i] != matrix[x][z].num[i]) {
-              n = 1;
-              break;
-            }
-            if(n == 0) {
-              q = x;
-              w = z;
-              n = 2;
-            }
-          }
-        }
-        if(n == 2) break;
-        z++;
-      }
-      if(n == 2) {
-        var z =0;
-        while(z < 9) {
-          if((matrix[x][z].name == 0) && (matrix[x][z].active != matrix[x][y].active) && (z != w)) {
-            var i = 0;
-            while(i < 9) {
-              if(matrix[x][z].num[i] == array[i]) matrix[x][z].num[i] = 0;
-              i++;
-            }
-          }
-          z++;
-        }
-      }
-      ////////////////По столбцу
-      var z = 0, n = 0, q = 0, w = 0;
-      while(z < 9) {
-        if((matrix[z][y].name == 0) && (matrix[z][y].active != matrix[x][y].active)) {
-          for(var i = 0; i < array.length; i++) {
-            if(array[i] != matrix[z][y].num[i]) {
-              n = 1;
-              break;
-            }
-            if(n == 0) {
-              q = x;
-              w = z;
-              n = 2;
-            }
-          }
-        }
-        if(n == 2) break;
-        z++;
-      }
-      if(n == 2) {
-        var z = 0;
-        while(z < 9) {
-          if((matrix[z][y].name == 0) && (matrix[z][y].active != matrix[x][y].active) && (z != q)) {
-            var i = 0;
-            while(i < 9) {
-              if(matrix[z][y].num[i] == array[i]) matrix[z][y].num[i] = 0;
-              i++;
-            }
-          }
-          z++;
-        }
-      }
-      //////////////////По секции
-    }
-    matrix[x][y].active = "false";
-  }
-  for(var x= 0; x < 9; x++) {
-    for(var y= 0; y < 9; y++) {
+  ////////////////////////////////////////////////////////////////////////////
+  for(let x= 0; x < 9; x++) {
+    for(let y= 0; y < 9; y++) {
       if(matrix[x][y] == 0) {
-        matrix[x][y] = new Inf(0, [1, 2, 3, 4, 5, 6, 7, 8, 9]);
+        matrix[x][y] = new Cell(0, [1, 2, 3, 4, 5, 6, 7, 8, 9]);
       }
-      else matrix[x][y] = new Inf(matrix[x][y], []);
+      else matrix[x][y] = new Cell(matrix[x][y], []);
     }
   }
-  for(var x= 0; x < 9; x++) {
-    for(var y= 0; y < 9; y++) {
+  for(let x= 0; x < 9; x++) {
+    for(let y= 0; y < 9; y++) {
       if(matrix[x][y].name == 0) {
-        matrix[x][y].num = analize(x, y);
-        matrix[x][y].name = update(x, y);
+        analize(x, y);
         if(matrix[x][y].name != 0) {
-          postAnalize(x, y);
           x = 0;
           y = 0;
         }
       }
     }
   }
-  for(var x= 0; x < 9; x++) {
-    for(var y= 0; y < 9; y++) {
+  for(let x= 0; x < 9; x++) {
+    for(let y= 0; y < 9; y++) {
       if(matrix[x][y].name == 0) {
-        matrix[x][y].num = deepAnalize(x, y);
-        nakedCouples(x, y, matrix);
-        matrix[x][y].name = update(x, y);
-        matrix[x][y].num = analize(x, y);
+        analize(x,y);
+        rowAnalize(x, y);
+        columnAnalize(x, y);
+        oneInPozition(x, y);
+        if(matrix[x][y].name == 0) {
+          update(x, y);
+        }
         if(matrix[x][y].name != 0) {
-          postAnalize(x, y);
+          postAnalize(x,y);
           x = 0;
           y = 0;
         }
       }
     }
   }
-  for(var x= 0; x < 9; x++) {
-    for(var y= 0; y < 9; y++) {
-      matrix[x][y] = matrix[x][y].name
+  for(let x= 0; x < 9; x++) {
+    for(let y= 0; y < 9; y++) {
+      matrix[x][y] = matrix[x][y].name;
     }
   }
   return matrix;
 }
+/*
+for(let x= 0; x < 9; x++) {
+  for(let y= 0; y < 9; y++) {
+    matrix[x][y] = matrix[x][y].name;
+  }
+}
+*/
